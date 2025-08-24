bf6c0fb93c062588d3bff9bdaf15e9d8809e4c3b
yousefQJ_20 <yousefnajeh3@gmail.com>
2025-08-24 18:54:11 +0300

    player-movement and physical layer for tileset

---
+++ colorsgame/character_body_2d.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+# Movement variables
+@export var speed: float = 200.0
+@export var jump_force: float = -400.0
+@export var gravity: float = 900.0
+
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		# Stop tiny gravity accumulation when grounded
+		velocity.y = 0 if velocity.y > 0 else velocity.y
+
+	# Reset horizontal velocity
+	velocity.x = 0
+
+	# Move left
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	# Move right
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+
+	# Jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_force
+
+	# Apply movement
+	move_and_slide()
